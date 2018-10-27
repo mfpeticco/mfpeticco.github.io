1ef40e560ec2cf629e9ec2696712b586d47a51a2
NLGGHZ <mfpeticco@yahoo.com>
2018-10-26 20:57:49 -0400

    Added Videos

--- specialpages/common.js
+++ specialpages/common.js
@@ -3,6 +3,16 @@ $(document).ready(function() {
     $('navbar ul').toggleClass('show');
   });
 
+jQuery(function() {
+    function setAspectRatio() {
+      jQuery('#demovideo').each(function() {
+        jQuery(this).css('height', jQuery(this).width() * 9/16);
+      });
+    }
+
+    setAspectRatio();   
+    jQuery(window).resize(setAspectRatio);
+});
 
   $('.slide').hide();
   $('.current').show();
